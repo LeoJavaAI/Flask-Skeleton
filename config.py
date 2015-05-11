5a9b91d9a6f804a7582c858069a6c9ce01d5b51f
Leo-g <leo@leog.in>
2015-05-11 21:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-password='qwedsa@123'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:{}@localhost:5432/test1'.format(password)
+password='password'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:{}@localhost:5432/database'.format(password)
