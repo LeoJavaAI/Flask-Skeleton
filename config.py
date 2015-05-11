17a45f3038f87fa718ffd5d391611aafc16756cc
Leo G <leo@leog.in>
2015-05-11 20:53:15 +0530

    Ini

---
+++ config.py
@@ -0,0 +1,2 @@
+password='qwedsa@123'
+SQLALCHEMY_DATABASE_URI='postgresql://postgres:{}@localhost:5432/test1'.format(password)
---
+++ run.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from app import app
+app.debug = True
+app.run(host='172.16.189.28')
