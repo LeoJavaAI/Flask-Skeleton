8fba5fd441547b1dd04376d0691fb5833c8e9613
Leo G <leo@leog.in>
2015-06-07 15:08:33 +0530

    scaffold final

--- scaffold.py
+++ scaffold.py
@@ -26,6 +26,7 @@ app_files = ['__init__.py', 'views.py', 'models.py']
 template_resource_path = os.path.join('app/templates',resources)
 template_files = ['add.html', '_form.html', 'index.html', 'update.html']
 
+#replace template content with new resource content 
 def generate(resource_path, file):
     with open(os.path.join(resource_path, file), 'a') as new_file:
         with open(os.path.join('scaffold/app/', file), 'r') as old_file:
@@ -33,6 +34,7 @@ def generate(resource_path, file):
                    new_file.write(line.format(resource=resource, resources=resources, Resources=resources.title()))
 
 
+#create the files for the resource
 def create_files(resource_path, files):
 
     if not os.path.isdir(resource_path):
@@ -51,11 +53,39 @@ def create_files(resource_path, files):
        print('{} already exists'.format(resource_path))
        return False       
        
-       
-#Create the files
-if create_files(app_resource_path, app_files) and create_files(template_resource_path, template_files) :
+ 
+#register blueprints
+def blueprint_register():
+    string_to_insert_after = '#Blueprints'
+    new_blueprint = """
+    #Blueprints
+    from app.{resources}.views import {resources}
+    app.register_blueprint({resources}, url_prefix='/{resources}')""".format(resources=resources)
+        
+    filedata = None
+    app_file = 'app/__init__.py'
+    with open(app_file, 'r+') as file:    
+        filedata = file.read()
+    if string_to_insert_after  in filedata:
+      #replace the first occurrence
+      filedata = filedata.replace(string_to_insert_after, new_blueprint,1)
+      with open(app_file, 'w') as file:
+        file.write(filedata)
+        print("Registered Blueprints for ",resources)
+        return True
+    else:
+      print("""Cannot find string #Blueprints in app/__init.py__", please ensure this file is similar to \n
+               https://github.com/Leo-g/Flask-Skeleton/blob/master/app/__init__.py""")
+      return False
+               
+      
+
+
+if create_files(app_resource_path, app_files) and create_files(template_resource_path, template_files) and blueprint_register() :
+    print ("created", resources)
     sys.exit(0)
 else:
+    print("running rm")
     for path in [app_resource_path, template_resource_path]:
       if os.path.isdir(path):
         shutil.rmtree(path)
