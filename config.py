118bc94006b64acafe1dfb3466d926a6e5128799
Leo G <leo@leog.in>
2015-06-26 19:21:21 +0530

    config secret

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 #DATABASE SETTINGS
 db_username = 'postgres'
 db_password = ''
-db_name = 'users'
+db_name = 'fscafold'
 db_hostname = 'localhost'
 
 DEBUG = True
 PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = True
-SECRET_KEY = ""
+SECRET_KEY = "SOME SECRET"
 SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER=db_username,
                                                                                         DB_PASS=db_password,
                                                                                         DB_ADDR=db_hostname,
