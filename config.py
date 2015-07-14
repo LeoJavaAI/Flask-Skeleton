137590e446de3bfbfb323e61b4d03ac259e99707
Leo G <leo@leog.in>
2015-07-14 20:49:44 +0530

    add mysql config to config.py

--- config.py
+++ config.py
@@ -1,15 +1,28 @@
 #DATABASE SETTINGS
-db_username = 'postgres'
-db_password = ''
-db_name = 'fscafold'
-db_hostname = 'localhost'
+pg_db_username = 'postgres'
+pg_db_password = ''
+pg_db_name = 'fscafold'
+pg_db_hostname = 'localhost'
+
+#MYSQL
+mysql_db_username = 'root'
+mysql_db_password = ''
+mysql_db_name = 'fscaffold'
+mysql_db_hostname = 'localhost'
 
 DEBUG = True
 PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = True
 SECRET_KEY = "SOME SECRET"
+###PostgreSQL
 SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER=db_username,
                                                                                         DB_PASS=db_password,
                                                                                         DB_ADDR=db_hostname,
                                                                                         DB_NAME=db_name)
+                                                                                        
+###MySQL
+"""SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER=mysql_db_username,
+                                                                                        DB_PASS=mysql_db_password,
+                                                                                        DB_ADDR=mysql_db_hostname,
+                                                                                        DB_NAME=mysql_db_name)"""
